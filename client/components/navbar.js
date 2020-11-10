68f198e97ea31d9bffe52c43715586772426e247
Jesse Swedlund <swedlundjesse@gmail.com>
2020-11-10 14:37:26 -0800

    CSS navbar update

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,23 +6,63 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
     <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
+      <div id="navBox">
+        <div id="logoBox">
+          <img id="logo" src="/images/logoOEI.png" />
+          <div id="title">Outdoor Equipment Incorporated</div>
         </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
+        <div id="rightSide">
+          <div id="searchLinkBox">
+            <div id="searchBox">
+              <form>
+                <input
+                  id="searchBar"
+                  type="text"
+                  placeholder="Search for great outdoor gear"
+                  size="40"
+                />
+              </form>
+              <button type="submit">
+                <i className="fa fa-search" />
+              </button>
+            </div>
+            {isLoggedIn ? (
+              <div>
+                {/* The navbar will show these links after you log in */}
+                <Link to="/home" className="navLink">
+                  Home
+                </Link>
+                <a href="#" onClick={handleClick} className="navLink">
+                  Logout
+                </a>
+                <Link to="/cart" className="navLink">
+                  Cart
+                </Link>
+              </div>
+            ) : (
+              <div>
+                {/* The navbar will show these links before you log in */}
+                <Link to="/login" className="navLink">
+                  Login
+                </Link>
+                <Link to="/signup" className="navLink">
+                  Sign Up
+                </Link>
+              </div>
+            )}
+          </div>
+          <div id="gearCategories">
+            <div className="gearLink">Tents</div>
+            <div className="gearLink">Footwear</div>
+            <div className="gearLink">Backpacks</div>
+            <div className="gearLink">Cooking</div>
+            <div className="gearLink">Clothing</div>
+            <div className="gearLink">Jackets</div>
+            <div className="gearLink">Hats</div>
+          </div>
         </div>
-      )}
+      </div>
     </nav>
     <hr />
   </div>
