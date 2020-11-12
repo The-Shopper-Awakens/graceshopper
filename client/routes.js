1281738e18171c6bd8c711acea7920f8ec885edb
Jesse Swedlund <swedlundjesse@gmail.com>
2020-11-12 13:08:06 -0800

    started with guestcart

--- client/routes.js
+++ client/routes.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Cart} from './components'
+import {Login, Signup, UserHome, Cart, GuestCart} from './components'
 import {me} from './store'
 import AllProducts from './components/AllProducts'
 import Product from './components/Product'
+
 /**
  * COMPONENT
  */
@@ -25,6 +26,7 @@ class Routes extends Component {
           <Route path="/signup" component={Signup} />
           <Route exact path="/products" component={AllProducts} />
           <Route path="/products/:productId" component={Product} />
+          <Route path="/cart" component={GuestCart} />
           {isLoggedIn && (
             <Switch>
               {/* Routes placed here are only available after logging in */}
