91865caff1b93787c255205a16aa3c93883360cb
Tim Samuel <timmatsam@gmail.com>
2020-11-12 16:11:05 -0500

    added two post routes (add/remove) in API

--- server/db/models/order_product.js
+++ server/db/models/order_product.js
@@ -1,10 +1,15 @@
-const {INTEGER} = require('sequelize')
 const Sequelize = require('sequelize')
 const db = require('../db')
 
 module.exports = db.define('Order_Product', {
-  quantity: INTEGER,
-  price: INTEGER
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 1
+  },
+  price: {
+    type: Sequelize.INTEGER
+  }
 })
 
 //create hook for price here
