a91e1073a47f4602824607b439b121b15b774a57
Jesse Swedlund <swedlundjesse@gmail.com>
2021-03-17 09:05:51 -0700

    changed db file

--- server/db/db.js
+++ server/db/db.js
@@ -3,12 +3,31 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
+let config
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    operatorsAliases: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+} else {
+  config = {
+    logging: false,
+    operatorsAliases: false
+  }
+}
+
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  {
-    logging: false,
-    ssl: true
-  }
+  config
 )
 module.exports = db
 
